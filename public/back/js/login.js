449a6095ad0ba209723940a8cb9278a7581a4c78
MaydayChan <maydayczy@163.com>
2018-12-15 17:21:12 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -8,16 +8,16 @@ $(function () {
         // 指定校验是的图标显示
         feedbackIcons: {
 
-            valid: 'glyphicon glyphicon-star',   // 有效的
+            valid: 'glyphicon glyphicon-ok',   // 有效的
             invalid: 'glyphicon glyphicon-remove',  // 无效的
-            validating: 'glyphicon glyphicon-ok',  // 确认的
+            validating: 'glyphicon glyphicon-refresh',  // 确认的
 
         },
 
 
         // 指定校验字段
         fields: {
-            // 校验用户名对应的name属性
+            // 校验用户名对应的name属性 
             username: {
                 validators: {
                     // 不能为空
@@ -31,11 +31,15 @@ $(function () {
                         message: '用户名长度必须在3到6之间'
                     },
 
-
+                    // callback 专门用来定制字段回调提示内容
+                    callback: {
+                        message: '用户名不存在哦'
+                    }
 
                 }
             },
 
+            // 校验密码子弹
             password: {
                 validators: {
                     // 不能为空
@@ -48,9 +52,12 @@ $(function () {
                         min: 3,
                         max: 10,
                         message: '密码长度必须为3到10之间'
+                    },
+
+                    // callback 专门用来定制字段的回调提示内容
+                    callback: {
+                        message: '密码错误哦'
                     }
-
-
                 }
             }
 
@@ -61,7 +68,53 @@ $(function () {
 
 
 
+    // 点击登录，发送ajax请求
+    // 根据后台返回的信息，分别进行处理
+
+    $(".form-horizontal").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+        $.ajax({
+
+            url: '/employee/employeeLogin',
+            type: 'post',
+            data: $('.form-horizontal').serialize(),
+            dataType: 'json',
+            success: function(info){
+                // console.log(info);
+                if(info.success) {
+                    // 验证成功，登录首页
+                    location.href = 'index.html';
+                }
+
+                if(info.error===1000) {
+                    // 用户名错误
+                    // 更新字段的状态   字段名称  状态  验证器   
+                    $('.form-horizontal').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback')
+                }
+
+                if(info.error===1001) {
+                    // 密码错误 
+                    $('.form-horizontal').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback')
+                    
+                }
+
+            }
+
+        })
+
+
+    });
+
+
+
+    // 点击重置按钮，重置表单，清空表单项，隐藏所有状态信息
+
+    $('.login .btn-reset').on('click',function(){
+
+        $('.form-horizontal').data('bootstrapValidator').resetForm()
+    })
+
+
+
 })
-
-
-// 登陆跳转功能
